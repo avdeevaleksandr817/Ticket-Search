393074b985cdfaa70486713666abb30a0eec2fed
avdeevaleksandr817 <avdeevaleksandr817@gmail.com>
2022-12-15 22:15:25 +0400

    comparator added fast

--- src/main/java/ru/netology/manager/TicketManager.java
+++ src/main/java/ru/netology/manager/TicketManager.java
@@ -8,6 +8,7 @@ import ru.netology.exception.NotFoundException;
 import ru.netology.repository.TicketRepository;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,7 +17,7 @@ public class TicketManager {
     private TicketRepository repository = new TicketRepository();
 
     // поиск билетов по аэропортам вылета-прилета
-    public TicketData[] findAll(String fromInput, String toInput) {
+    public TicketData[] findAll(String fromInput, String toInput, Comparator<TicketData> comparator) {
         TicketData[] results = new TicketData[0];
         for (TicketData ticket : repository.findAll()) {
             if (matchesFromTo(ticket, fromInput, toInput)) {
@@ -30,7 +31,7 @@ public class TicketManager {
         if (results.length == 0) {
             throw new NotFoundException("Билетов из " + fromInput + " в " + toInput + " не найдено!");
         }
-        Arrays.sort(results);
+        Arrays.sort(results, comparator);
         return results;
     }
 
